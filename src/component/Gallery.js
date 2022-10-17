de6f16d18a6f46e5aa3053b2f8d24b1524afa0f1
khoale <kthedestroy@gmail.com>
2022-10-17 21:11:32 +0700

    update new Grid layout for Gallery

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,66 +1,79 @@
 import React, { useMemo } from 'react'
-import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import Skeleton from '@mui/material/Skeleton';
 import { SKELETON_AMOUNT } from '../config/contants';
 
 const Gallery = ({ images, isLoading, message }) => {
 
-    const skeletonArray = useMemo(()=>{
-        return Array.from(Array(SKELETON_AMOUNT).keys()).map((item, index)=>{
-            return <Skeleton key={index} animation="wave" variant="rounded" width={200} height={180} />
+    const skeletonArray = useMemo(() => {
+        return Array.from(Array(SKELETON_AMOUNT).keys()).map((item, index) => {
+            return (
+                <Grid
+                    item
+                    xs={6} sm={4} md={3}
+                    key={index}
+                >
+                     <Skeleton key={index} animation="wave" variant="rounded" height={200}/>
+                </Grid>
+            )
         })
-    },[])
+    }, [])
 
     return (
         <Paper
             variant="outlined"
             sx={{
-                width: "80%",
+                width: "100%",
+                padding: "1rem"
             }}
         >
-            <ImageList
-                sx={{
-                    padding: "1rem",
-                }}
-                cols={4} gap={12}
+            <Grid
+                container
+                spacing={{ xs: 1, md: 2 }}
             >
-                {(isLoading) ? skeletonArray : (
+                {isLoading ? skeletonArray : (
                     <>
                         {images.map((item) => (
-                            <ImageListItem key={item.id}>
-                                <a
-                                    href={item.url}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    <Box
-                                        sx={{
-                                            padding: "1rem",
-                                            border: "1px solid rgba(0,0,0,0.1)",
-                                            cursor: 'pointer',
-                                            "&:hover": {
-                                                border: "1px solid #333"
-                                            }
-                                        }}
+                            <Grid
+                                item
+                                key={item.id}
+                                xs={6} sm={4} md={3}
+                            >
+                                <ImageListItem key={item.id}>
+                                    <a
+                                        href={item.url}
+                                        target="_blank"
+                                        rel="noreferrer"
                                     >
-                                        <img
-                                            src={`${item.url}`}
-                                            srcSet={`${item.url}`}
-                                            alt={item.name}
-                                            loading="lazy"
-                                        />
-                                    </Box>
-                                </a>
-                            </ImageListItem>
+                                        <Box
+                                            sx={{
+                                                padding: "1rem",
+                                                border: "1px solid rgba(0,0,0,0.1)",
+                                                cursor: 'pointer',
+                                                "&:hover": {
+                                                    border: "1px solid #333"
+                                                }
+                                            }}
+                                        >
+                                            <img
+                                                src={`${item.url}`}
+                                                srcSet={`${item.url}`}
+                                                alt={item.name}
+                                                loading="lazy"
+                                            />
+                                        </Box>
+                                    </a>
+                                </ImageListItem>
+                            </Grid>
                         ))}
                     </>
-                )
-                }
+                )}
 
-            </ImageList >
+
+            </Grid>
         </Paper>
     )
 }
